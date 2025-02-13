6ec90237c38312ff671e93e1cf7c2ee2629b33b3
Emil Zackrisson <emil.zackrisson@gmail.com>
2025-02-13 16:28:21 +0100

    Added StatusWhenEmpty to DirectoryExistsCheck.cs

--- RepositoryLinter/Checks/DirectoryExistsCheck.cs
+++ RepositoryLinter/Checks/DirectoryExistsCheck.cs
@@ -7,9 +7,21 @@ namespace RepositoryLinter.Checks;
 /// <param name="pathToGitDirectory">Path to the local copy of the Git repository.</param>
 public class DirectoryExistsCheck(string relativeDirectoryPath, string pathToGitDirectory) : Checker
 {
+    public CheckStatus StatusWhenEmpty { get; init; } = CheckStatus.Green;
     public override void Run()
     {
         var exists = Directory.Exists(Path.Join(pathToGitDirectory,relativeDirectoryPath));
+
+        if (exists)
+        {
+            var empty = Directory.GetFiles(Path.Join(pathToGitDirectory, relativeDirectoryPath)).Length != 0;
+            
+            if (empty)
+            {
+                Status = StatusWhenEmpty;
+                return;
+            }
+        }
         
         Status = exists ? CheckStatus.Green : StatusWhenFailed;
     }
