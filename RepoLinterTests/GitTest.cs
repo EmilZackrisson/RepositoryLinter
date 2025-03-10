d45fd8ed7565b38b36900daf3abfc08bada04aa8
Emil Zackrisson <emil.zackrisson@gmail.com>
2025-03-10 11:25:09 +0100

    Update test to clone a different Git repository

--- RepoLinterTests/GitTest.cs
+++ RepoLinterTests/GitTest.cs
@@ -10,14 +10,14 @@ public class GitTest
     private readonly ITestOutputHelper _testOutputHelper;
     private readonly string _path;
     private readonly GlobalConfiguration _config = new();
-    
+
     public GitTest(ITestOutputHelper testOutputHelper)
     {
         _testOutputHelper = testOutputHelper;
         _path = Path.Join(Path.GetTempPath(), Path.GetRandomFileName());
         InitFakeRepo();
     }
-    
+
     ~GitTest()
     {
         Directory.Delete(_path, true);
@@ -26,10 +26,10 @@ public class GitTest
     private void InitFakeRepo()
     {
         Directory.CreateDirectory(_path);
-        
+
         // Create a .gitingore file and add a file to ignore
         File.WriteAllText(Path.Join(_path, ".gitignore"), "ignored.txt");
-        
+
         var initialized = RunGitCommand(_path, "init");
         if (!initialized)
         {
@@ -38,19 +38,19 @@ public class GitTest
 
         var x = RunGitCommand(_path, "config --local user.email \"test@example.com\"");
         var y = RunGitCommand(_path, "config --local user.name \"Test\"");
-        
+
         if (!x || !y)
         {
             throw new GitException("Failed to set git user");
         }
-        
+
         // Create a commit
         File.WriteAllText(Path.Join(_path, "README.md"), "# Hello World");
         RunGitCommand(_path, "add .");
         RunGitCommand(_path, "branch -m main");
         RunGitCommand(_path, "commit -m Hello");
     }
-    
+
     private static bool RunGitCommand(string path, string command)
     {
         var process = new Process
@@ -65,13 +65,13 @@ public class GitTest
                 CreateNoWindow = true
             }
         };
-        
+
         var started = process.Start();
         if (!started)
         {
             throw new Exception("Failed to start git command");
         }
-        
+
         process.WaitForExit();
         return process.ExitCode == 0;
     }
@@ -79,27 +79,27 @@ public class GitTest
     [Fact]
     public void CloneGitRepository()
     {
-        var git = new Git(new Uri("https://github.com/EmilZackrisson/TraefikAPI"), _config);
+        var git = new Git(new Uri("https://github.com/githubtraining/hellogitworld"), _config);
         git.Clone();
         var directory = Path.Join(git.ParentDirectory, git.RepositoryName);
         _testOutputHelper.WriteLine("Git repository cloned to: " + directory);
         Assert.True(Directory.Exists(directory));
     }
-    
+
     [Fact]
     public void CreateGitObjectWithLocalPath()
     {
         Git git = new Git(_path);
         Assert.Equal(_path, Path.Join(git.ParentDirectory, git.RepositoryName));
     }
-    
+
     [Fact]
     public void CreateGitObjectWithLocalPathThatDoesNotExist()
     {
         var path = Path.Join(_path, Path.GetRandomFileName());
         Assert.Throws<DirectoryNotFoundException>(() => new Git(path));
     }
-    
+
     [Fact]
     public void CreateGitObjectWithLocalPathThatIsNotAGitRepository()
     {
@@ -114,7 +114,7 @@ public class GitTest
         var git = new Git(_path);
         Assert.Equal(1, git.GetCommitCount());
     }
-    
+
     [Fact]
     public void GetContributors()
     {
@@ -131,7 +131,7 @@ public class GitTest
         var ignored = ignore.IsIgnored(pathToIgnoredFile);
         Assert.True(ignored);
     }
-    
+
     [Fact]
     public void ShouldNotIgnoreFile()
     {
@@ -140,6 +140,4 @@ public class GitTest
         var ignored = ignore.IsIgnored(pathToNotIgnoredFile);
         Assert.False(ignored);
     }
-
-    
 }
